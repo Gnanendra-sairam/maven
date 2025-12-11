fab7c3be4967922e290cf62f94447d2e5698c26f
Gnanendra-sairam <sairam.gutlapalli@gmail.com>
2025-12-11 11:12:33 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static String message() {
+        return "Jenkins Maven Build Success!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(message());
+    }
+}
